d73fb623cc1b40e4f63f55eec2ea29a9ae8067a2
kasarla sai vivek <vivekkasarla781@gmail.com>
2024-07-16 09:29:45 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+my_dict={'name':'john','age':21,'nationality':'UK','occupation':"web developer"}
+#add key value pair into my_dict
+my_dict['salary']=10000
+#remove key value pairs from the dictionary
+my_dict.pop('nationality')
+# to modifty value of a key
+my_dict['age']=40
+#To display key and values
+print(my_dict.keys())
+print(my_dict.values())
+#print all the elements in the dictionary
+print(my_dict)
